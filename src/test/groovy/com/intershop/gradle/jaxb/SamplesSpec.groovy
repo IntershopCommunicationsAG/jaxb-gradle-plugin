aed8572f06099d2a7543ed9cb0e5ca87c8b62da7
mraab <mraab@intershop.de>
2016-08-01 11:31:15 +0200

    add test case for multi threaded execution

--- src/test/groovy/com/intershop/gradle/jaxb/SamplesSpec.groovy
+++ src/test/groovy/com/intershop/gradle/jaxb/SamplesSpec.groovy
@@ -22,6 +22,73 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS as SUCCESS
 
 class SamplesSpec extends AbstractIntegrationSpec {
 
+    def 'Test multithread execution - xjc'() {
+        given:
+        copyResources('samples/catalog-resolver', 'test01')
+        copyResources('samples/create-marshal', 'test02')
+        copyResources('samples/external-customize', 'test03')
+        copyResources('samples/fix-collides', 'test04')
+        copyResources('samples/synchronized-methods', 'test05')
+
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'com.intershop.gradle.jaxb'
+            }
+
+            jaxb {
+                javaGen {
+                    test01 {
+                        schema = file('test01/test.xsd')
+                        catalog = file('test01/catalog.cat')
+                        packageName = ''
+                    }
+                    test02 {
+                        packageName = 'primer.po'
+                        schema = file('test02/po.xsd')
+                    }
+                    test03 {
+                        binding = file('test03/binding.xjb')
+                        schema = file('test03/po.xsd')
+                    }
+                    test04 {
+                        binding = file('test04/binding.xjb')
+                        schema = file('test04/example.xsd')
+                    }
+                    test05 {
+                        schema = file('test05/po.xsd')
+                        packageName = 'primer.test05.myPo'
+                        extension = true
+                        arg('-Xsync-methods')
+                    }
+                }
+            }
+
+            repositories {
+                mavenCentral()
+            }
+        """.stripIndent()
+
+        when:
+        List<String> args = ['compileJava', '-s', '-i', '--configure-on-demand', '--parallel', '--max-workers=4']
+
+        def result = getPreparedGradleRunner()
+                .withArguments(args)
+                .withGradleVersion(gradleVersion)
+                .build()
+
+        then:
+        result.task(':jaxbJavaGenTest01').outcome == SUCCESS
+        result.task(':jaxbJavaGenTest02').outcome == SUCCESS
+        result.task(':jaxbJavaGenTest03').outcome == SUCCESS
+        result.task(':jaxbJavaGenTest04').outcome == SUCCESS
+        result.task(':jaxbJavaGenTest05').outcome == SUCCESS
+        result.task(':compileJava').outcome == SUCCESS
+
+        where:
+        gradleVersion << supportedGradleVersions
+    }
+
     def 'Test catalog-resolver - xjc'() {
         given:
         copyResources('samples/catalog-resolver')
