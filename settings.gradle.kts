fc381b84edbf90dd08b54a9b3922d7411fef2dde
Matthias Raab <m.raab@intershop.de>
2021-05-27 19:15:07 +0200

    solve issue with dokka lib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "jaxb-gradle-plugin"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        // for org/jetbrains/kotlinx/kotlinx-html-jvm/0.7.2/kotlinx-html-jvm-0.7.2.pom
+        jcenter()
+    }
+}
